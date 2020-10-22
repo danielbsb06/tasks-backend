5f74791b6e6692d14adbe7c074777fb3fe5c4269
Daniel Alves <danielbsb06@gmail.com>
2020-10-22 20:03:43 -0300

    cria classe teste DateUtils

---
+++ src/test/java/br.ce.wcaquino.taskbackend.utils/DateUtilsTest.java
@@ -0,0 +1,21 @@
+package br.ce.wcaquino.taskbackend.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+public class DateUtilsTest {
+
+    @Test
+    public void deveRetornarTrueParaDatasFuturas(){
+        LocalDate date = LocalDate.of(2030, 01,01);
+        Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+    }
+
+    @Test
+    public void deveRetornarFalseParaDatasPassadas(){
+        LocalDate date = LocalDate.of(2000, 01,01);
+        Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));
+    }
+}
