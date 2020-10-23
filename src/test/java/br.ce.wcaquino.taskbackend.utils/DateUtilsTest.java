3db63792e93e2b1f68b4922848a0978ebaabee6d
Daniel Alves <danielbsb06@gmail.com>
2020-10-23 07:40:47 -0300

    cria testes data presente e passado

--- src/test/java/br.ce.wcaquino.taskbackend.utils/DateUtilsTest.java
+++ src/test/java/br.ce.wcaquino.taskbackend.utils/DateUtilsTest.java
@@ -9,13 +9,20 @@ public class DateUtilsTest {
 
     @Test
     public void deveRetornarTrueParaDatasFuturas(){
-        LocalDate date = LocalDate.of(2030, 01,01);
+        LocalDate date = LocalDate.now().plusYears(1);
+        Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+    }
+
+    @Test
+    public void deveRetornarTrueParaDataAtual(){
+        LocalDate date = LocalDate.now();
         Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
     }
 
     @Test
     public void deveRetornarFalseParaDatasPassadas(){
-        LocalDate date = LocalDate.of(2000, 01,01);
+        LocalDate date = LocalDate.now().plusYears(-1);
         Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));
     }
+
 }
